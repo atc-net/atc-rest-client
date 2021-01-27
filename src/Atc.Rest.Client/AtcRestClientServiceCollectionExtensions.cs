61934c1da3c4897737451e774f86f7f346404576
Egil Hansen <egil@assimilated.dk>
2021-01-27 15:43:59 +0000

    Removed auth logic from client. Closes #1.

--- src/Atc.Rest.Client/AtcRestClientServiceCollectionExtensions.cs
+++ src/Atc.Rest.Client/AtcRestClientServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 using System;
-using Atc.Rest.Client.Authentication;
 using Atc.Rest.Client.Builder;
 using Atc.Rest.Client.Options;
 using Atc.Rest.Client.Serialization;
@@ -18,10 +17,6 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddSingleton(options);
             services.AddSingleton<AtcRestClientOptions>(options);
 
-            // Configure HttpClient and authorization
-            services.AddSingleton<IBearerTokenProvider, BearerTokenProvider>();
-            services.AddTransient<AzureAuthenticationHandler>();
-
             var clientBuilder = services.AddHttpClient(clientName, (s, c) =>
             {
                 var o = s.GetRequiredService<AtcRestClientOptions>();
@@ -29,7 +24,6 @@ namespace Microsoft.Extensions.DependencyInjection
                 c.Timeout = o.Timeout;
             });
 
-            clientBuilder.AddHttpMessageHandler<AzureAuthenticationHandler>();
             httpClientBuilder?.Invoke(clientBuilder);
 
             // Register utilities
