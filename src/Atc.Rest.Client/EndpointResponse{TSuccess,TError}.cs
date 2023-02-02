863275984c5f3d0c180b39306064086c8e60da45
Christian Helle <christian.helle@outlook.com>
2023-02-02 18:09:51 +0100

    Move IEndpointResponse implementation types to separate files

---
+++ src/Atc.Rest.Client/EndpointResponse{TSuccess,TError}.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Atc.Rest.Client
+{
+    public class EndpointResponse<TSuccess, TError>
+        : EndpointResponse
+        where TSuccess : class
+        where TError : class
+    {
+        public EndpointResponse(EndpointResponse response)
+            : base(response)
+        {
+        }
+
+        public EndpointResponse(
+            bool isSuccess,
+            HttpStatusCode statusCode,
+            string content,
+            object? contentObject,
+            IReadOnlyDictionary<string, IEnumerable<string>> headers)
+            : base(isSuccess, statusCode, content, contentObject, headers)
+        {
+        }
+
+        public TSuccess? SuccessContent => IsSuccess ? CastContent<TSuccess>() : null;
+
+        public TError? ErrorContent => !IsSuccess ? CastContent<TError>() : null;
+    }
+}
---
+++ src/Atc.Rest.Client/EndpointResponse{TSuccess}.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Atc.Rest.Client
+{
+    public class EndpointResponse<TSuccess>
+        : EndpointResponse
+        where TSuccess : class
+    {
+        public EndpointResponse(EndpointResponse response)
+            : base(response)
+        {
+        }
+
+        public EndpointResponse(
+            bool isSuccess,
+            HttpStatusCode statusCode,
+            string content,
+            TSuccess? contentObject,
+            IReadOnlyDictionary<string, IEnumerable<string>> headers)
+            : base(isSuccess, statusCode, content, contentObject, headers)
+        {
+        }
+
+        public TSuccess? SuccessContent => IsSuccess ? CastContent<TSuccess>() : null;
+    }
+}
