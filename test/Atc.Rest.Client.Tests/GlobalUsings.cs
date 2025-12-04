93cf3e6b8dd4186a3767ad01843999b3114c384c
davidkallesen <david@lmdk.dk>
2025-12-04 14:02:54 +0100

    chore: cleanup in usings

--- test/Atc.Rest.Client.Tests/GlobalUsings.cs
+++ test/Atc.Rest.Client.Tests/GlobalUsings.cs
@@ -3,4 +3,6 @@ global using System.Net.Http.Headers;
 global using System.Runtime.Serialization;
 global using System.Text.Json;
 global using Atc.Rest.Client.Builder;
+global using Atc.Rest.Client.Options;
 global using Atc.Rest.Client.Serialization;
+global using Microsoft.Extensions.DependencyInjection;
